fc7de38c7c8987206516964f1f8edb9157f0de67
Jonathan <jonathanwoollettlight@gmail.com>
2023-11-15 21:31:46 +0000

    trim

--- nn/ep2/0.py
+++ nn/ep2/0.py
@@ -12,8 +12,10 @@ from ecommon import (
 from manim import *
 
 from manim_voiceover import VoiceoverScene
+
 # from manim_voiceover.services.gtts import GTTSService
 from manim_voiceover.services.recorder import RecorderService
+
 
 # Convolution
 class EpisodeScene(VoiceoverScene):
